91daf7a427ac0c81ea2b1829e802000206256e6b
sallby <144801648+sallby@users.noreply.github.com>
2024-02-19 16:39:15 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
+resource "random_pet" "pet" {
+  length    = 2
+  separator = "-"
+}
+
 resource "azurerm_resource_group" "rg_name" {
-  name     = "${var.nomRessource}-$(uuid())"
+  name     = "${var.nomRessource}-${random_pet.pet.id}"
   location = var.localisation
 }
