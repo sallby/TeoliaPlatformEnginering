756bab573fd69b704edd3c58c65341baaf4a7014
sallby <djiby-oumar.sall@teolia.fr>
2024-03-20 15:59:00 +0100

    débug terraform validate

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,8 @@ module "rg" {
   localisation = var.localisation
 }
 
-resource "azurerm_policy_assignment" "location_policy" {
-  name                 = "location-policy"
-  scope                = module.rg.resource_group_id
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/01be5988-e43e-45f8-a060-172570bc6981"
-}
+#resource "azurerm_policy_assignment" "location_policy" {
+#  name                 = "location-policy"
+#  scope                = module.rg.resource_group_id
+#  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/01be5988-e43e-45f8-a060-172570bc6981"
+#}
