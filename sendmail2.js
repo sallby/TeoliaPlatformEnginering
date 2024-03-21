6e8d0facb8e8a140a8d77860d65802aab27f7c4e
sallby <djiby-oumar.sall@teolia.fr>
2024-03-21 16:06:29 +0100

    update function sendmail

--- sendmail2.js
+++ sendmail2.js
@@ -1,28 +1,22 @@
 const { app } = require("@azure/functions");
 const fetch = require('node-fetch');
+const msal = require('@azure/msal-node');
+
 // ID client et secret client du service principal
 const clientId = process.env.AZURE_CLIENT_ID;
 const clientSecret = process.env.AZURE_CLIENT_SECRET;
 
-const token_access = process.env.ACCESS;
+// Créez une configuration pour MSAL
+const config = {
+    auth: {
+        clientId: clientId,
+        authority: "https://login.microsoftonline.com/c6cc03ce-c0c9-4397-a535-2a67c8d16335",
+        clientSecret: clientSecret
+    }
+};
 
-// Fonction pour obtenir un jeton d'accès à partir de l'endpoint d'autorisation d'Azure AD
-async function getTokenFromAAD() {
-    const response = await fetch(`https://login.microsoftonline.com/c6cc03ce-c0c9-4397-a535-2a67c8d16335/oauth2/v2.0/token`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-            grant_type: 'client_credentials',
-            client_id: clientId,
-            client_secret: clientSecret,
-            scope: 'https://graph.microsoft.com/.default',
-        }),
-    });
-    return await response.json();
-}
-
+// Créez une nouvelle instance de client
+const cca = new msal.ConfidentialClientApplication(config);
 
 async function sendmail(req, context) {
     context.res = {
@@ -36,19 +30,20 @@ async function sendmail(req, context) {
             "Content-Type": "application/json"
         },
     };
-    const data = await req.json();
+    const clientCredentialRequest = {
+        scopes: ["https://graph.microsoft.com/.default"], // scopes indiquent les ressources auxquelles l'application demande l'accès
+    };
+
+    const data = req.body;
 
     const messageContent = `Bonjour ${data.prenom || ''}, Votre demande a été refusée. Veuillez nous contacter pour plus d'informations.`;
+
     try {
-
-        // Obtenez un jeton d'accès à partir de l'endpoint d'autorisation d'Azure AD
-        const tokenResponse = await getTokenFromAAD();
-
-        // Utilisez le jeton d'accès pour accéder à l'API Microsoft Graph
-        const accessToken = tokenResponse.access_token;
+        const response = await cca.acquireTokenByClientCredential(clientCredentialRequest);
+        const token_access = response.accessToken;
 
         // Envoyez une requête à l'API Microsoft Graph en utilisant le jeton d'accès
-        const response = await fetch(`https://graph.microsoft.com/v1.0/me/sendMail`, {
+        const responseMail = await fetch(`https://graph.microsoft.com/v1.0/me/sendMail`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -73,21 +68,20 @@ async function sendmail(req, context) {
             }),
         });
 
-        if (!response.ok) {
-            throw new Error(`Erreur lors de la requête: ${response.status} ${response.statusText}`);
+        if (!responseMail.ok) {
+            throw new Error(`Erreur lors de la requête: ${responseMail.status} ${responseMail.statusText}`);
         }
 
         context.res = {
             body: 'Mail envoyé avec succès.',
         };
     } catch (error) {
-        console.error('Erreur lors de la requête:', error.message);
+        console.log(error);
         context.res = {
             status: 500,
-            body: 'Une erreur s\'est produit.',
+            body: 'Une erreur est survenue lors de l\'envoi du mail.',
         };
     }
-
 }
 app.http('sendmail', {
     methods: ['POST'],
