a6abd9f7affc5476075007b422ab5eea9e1f11f2
sallby <djiby-oumar.sall@teolia.fr>
2024-04-08 17:24:53 +0200

    test add cost resource group

--- api/src/functions/sendmail.js
+++ api/src/functions/sendmail.js
@@ -1,28 +1,17 @@
 const { app } = require("@azure/functions");
 const fetch = require('node-fetch');
-// ID client et secret client du service principal
-const clientId = process.env.AZURE_CLIENT_ID;
-const clientSecret = process.env.AZURE_CLIENT_SECRET;
+const axios = require('axios');
 
-const token_access = process.env.ACCESS;
-
-// Fonction pour obtenir un jeton d'accès à partir de l'endpoint d'autorisation d'Azure AD
 async function getTokenFromAAD() {
-    const response = await fetch(`https://login.microsoftonline.com/c6cc03ce-c0c9-4397-a535-2a67c8d16335/oauth2/v2.0/token`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-            grant_type: 'client_credentials',
-            client_id: clientId,
-            client_secret: clientSecret,
-            scope: 'https://graph.microsoft.com/.default',
-        }),
-    });
-    return await response.json();
+    try {
+        // Appeler votre API pour récupérer le jeton d'accès depuis Azure AD
+        const tokenResponse = await axios.post('http://localhost:7071/api/getTokenFromAAD');
+        return tokenResponse.data.access_token;
+    } catch (error) {
+        console.error('Erreur lors de la récupération du token:', error.message);
+        throw error;
+    }
 }
-
 
 async function sendmail(req, context) {
     context.res = {
@@ -36,23 +25,20 @@ async function sendmail(req, context) {
             "Content-Type": "application/json"
         },
     };
-    const data = await req.json();
 
-    const messageContent = `Bonjour ${data.prenom || ''}, Votre demande a été refusée. Veuillez nous contacter pour plus d'informations.`;
     try {
+        // Obtenez le jeton d'accès
+        const token = await getTokenFromAAD();
 
-        // Obtenez un jeton d'accès à partir de l'endpoint d'autorisation d'Azure AD
-        const tokenResponse = await getTokenFromAAD();
-
-        // Utilisez le jeton d'accès pour accéder à l'API Microsoft Graph
-        const accessToken = tokenResponse.access_token;
+        const data = await req.json();
+        const messageContent = `Bonjour ${data.prenom || ''}, Votre demande a été refusée. Veuillez nous contacter pour plus d'informations.`;
 
         // Envoyez une requête à l'API Microsoft Graph en utilisant le jeton d'accès
         const response = await fetch(`https://graph.microsoft.com/v1.0/me/sendMail`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token_access}`,
+                'Authorization': `Bearer ${token}`,
             },
             body: JSON.stringify({
                 message: {
@@ -84,10 +70,9 @@ async function sendmail(req, context) {
         console.error('Erreur lors de la requête:', error.message);
         context.res = {
             status: 500,
-            body: 'Une erreur s\'est produit.',
+            body: 'Une erreur s\'est produite.',
         };
     }
-
 }
 app.http('sendmail', {
     methods: ['POST'],
